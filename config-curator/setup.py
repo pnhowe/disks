7e5170119704c5d3cdf0be9ff7dbdf7d0b052114
Peter Howe <pnhowe@gmail.com>
2020-08-01 14:00:53 -0600

    updating config-curator

--- config-curator/setup.py
+++ config-curator/setup.py
@@ -6,6 +6,6 @@ setup( name='config-curator',
        description='Config Curator',
        author='Peter Howe',
        version='0.1',
-       author_email='peter.howe@virtustream.com',
-       packages=[ 'config_curator', 'config_curator.controller', 'libconfig', 'libconfig.jinja2', 'libconfig.providers', 'libconfig.markupsafe' ],
+       author_email='pnhowe@gmail.com',
+       packages=[ 'config_curator', 'config_curator.contractor', 'config_curator.contractor.cinp', 'libconfig', 'libconfig.jinja2', 'libconfig.providers', 'libconfig.markupsafe' ],
        )
